2bc64ce06a0c3fa83064eb5bd1ec2649b8b3645a
guxiaowu <337118043@qq.com>
2020-02-13 13:40:03 +0800

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Render from './components/Render';
-
+import { Button } from 'antd';
 const App = () => {
   return (
     <div className="App">

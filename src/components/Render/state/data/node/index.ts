f84c1cf7b1ff09c98ddbdce150b5f2b7481b9a45
guxiaowu <337118043@qq.com>
2020-02-15 14:29:26 +0800

    add node配置widget

--- src/components/Render/state/data/node/index.ts
+++ src/components/Render/state/data/node/index.ts
@@ -1,17 +1,38 @@
 import { getNewID } from '../../id';
+import Widegt from '../widget';
 class Node {
     private id : number;
     private x : number;
     private y : number;
     private nodeType : number;        // 节点类型
-    private renderList : Array<number>;  // 渲染类型
-    constructor(x: number,y : number,nodeType : number,renderList : Array<number>) {
+    private widgets : Array<Widegt>;  // 渲染类型
+    private canReach : boolean = true;
+    constructor(x: number,y : number,nodeType : number) {
         this.id = getNewID(); 
         this.x = x;
         this.y = y;
         this.nodeType = nodeType;
-        this.renderList = renderList;
+        this.widgets = [];
     }
+    
+    addWidget(w:Widegt) {
+        this.canReach = this.canReach || w.getCanReach();
+        let pos = -1;
+        this.widgets.some((v,i)=>{
+            const flag = v.getWidgetType() == w.getWidgetType();
+            if(flag) {
+                pos = i;
+            }
+            return flag;
+        })
+        if(pos < 0){
+            this.widgets.push(w);
+            this.widgets.sort((a,b)=>a.getZ()- b.getZ());
+        } else {
+            this.widgets[pos] = w;
+        }
+    }
+
     render() {
 
     }
--- src/components/Render/state/data/widget/index.ts
+++ src/components/Render/state/data/widget/index.ts
@@ -1,16 +1,33 @@
 import { getNewID } from '../../id';
 class Widget {
     private id : number;
-    private parent : number;    //父节点id
-    private type : number;
-    private widgetType : number; //在widget下继续的细分，比如grass有多种
+    private parent : number = 0;    //父节点id
+    private type : number;//在widget下继续的细分，比如grass有多种
+    private widgetType : number; 
     private z : number ;         //渲染纵轴
-    constructor(parent:number,type:number,widgetType : number,z:number){
+    private canReach : boolean = true;
+    constructor(type:number,widgetType : number,z:number,canReach:boolean){
         this.id = getNewID()
-        this.parent = parent;
         this.type = type;
         this.widgetType = widgetType;
         this.z = z;
+        this.canReach = canReach;
+    }
+
+    setParent(p:number) {
+        this.parent = p;
+    }
+    getParent(){
+        return this.parent;
+    }
+    getCanReach() {
+        return this.canReach;
+    }
+    getWidgetType(){
+        return this.widgetType;
+    }
+    getZ(){
+        return this.z;
     }
 }
 

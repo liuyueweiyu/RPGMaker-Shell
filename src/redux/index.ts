56ccd964644dd40c055e51d2f3903a043674f947
guxiaowu <337118043@qq.com>
2020-02-16 19:45:24 +0800

    add 配置redux的chrome插件

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,10 @@
-import { createStore ,Reducer} from 'redux';
+// import { applyMiddleware, createStore } from 'redux';
+import { createStore } from 'redux';
 import { reducer } from './reducer';
+// import {createLogger} from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // @ts-ignore
-const store = createStore(reducer);
-
+const store = createStore(reducer,composeWithDevTools());
 
 export default store

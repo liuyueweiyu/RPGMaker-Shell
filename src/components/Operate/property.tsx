e036c17594c28bb129eecf2ac066f227da5e465a
guxiaowu <337118043@qq.com>
2020-02-22 19:57:12 +0800

    add 属性面板添加测试输出

--- src/components/Operate/property.tsx
+++ src/components/Operate/property.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { WINDOW_PROPERTY_WIDTH,WINDOW_MENU_HEIGHT } from '../Render/constant/window';
+import store from '../../redux';
 function Property() {
+    const [property,setProperty] = useState("");
+    store.subscribe(()=>{
+        const p = JSON.stringify(store.getState().selectedNodes);
+        if(p !== property) {
+            setProperty(p);
+        }
+    })
     return (
         // tslint:disable-next-line: jsx-self-close
-        <div style={{width:WINDOW_PROPERTY_WIDTH,float:'right',height:`calc(100vh - ${WINDOW_MENU_HEIGHT}px)`,borderLeft:'1px solid #e8e8e8',boxSizing:"border-box"}}>
+        <div 
+            // tslint:disable-next-line: jsx-no-multiline-js
+            style={
+                {
+                    width:WINDOW_PROPERTY_WIDTH,
+                    float:'right',
+                    wordBreak : 'break-all',
+                    height:`calc(100vh - ${WINDOW_MENU_HEIGHT}px)`,
+                    borderLeft:'1px solid #e8e8e8',
+                    boxSizing:"border-box"
+                }
+            }
+        >
+            <p>{property}</p>
         </div>
     )
 }
